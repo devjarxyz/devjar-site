444a4cc39ed8a9f4c71a80c10128b4b0a1f43511
Vlad <vladimir.strukelj@gmail.com>
2020-10-17 15:41:37 +0200

    stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,39 @@
-import Router from "next/router";
-import withGA from "next-ga";
+// import Router from "next/router";
+// import withGA from "next-ga";
 
-function MyApp({ Component, pageProps }) {
+// function MyApp({ Component, pageProps }) {
    
-    return <Component {...pageProps} />
-  }
+//     return <Component {...pageProps} />
+//   }
   
-  // Only uncomment this method if you have blocking data requirements for
-  // every single page in your application. This disables the ability to
-  // perform automatic static optimization, causing every page in your app to
-  // be server-side rendered.
-  //
-  // MyApp.getInitialProps = async (appContext) => {
-  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  //   const appProps = await App.getInitialProps(appContext);
-  //
-  //   return { ...appProps }
-  // }
+//   // Only uncomment this method if you have blocking data requirements for
+//   // every single page in your application. This disables the ability to
+//   // perform automatic static optimization, causing every page in your app to
+//   // be server-side rendered.
+//   //
+//   // MyApp.getInitialProps = async (appContext) => {
+//   //   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   //   const appProps = await App.getInitialProps(appContext);
+//   //
+//   //   return { ...appProps }
+//   // }
 
 
   
-  export default withGA("UA-99740932-2", Router)(MyApp);
+//   export default withGA("UA-99740932-2", Router)(MyApp);
+
+import React, { Fragment } from 'react'
+import Router from 'next/router'
+
+import * as gtag from '../components/lib/gtag'
+
+// Notice how we track pageview when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+
+export default ({ Component, pageProps }) => {
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+    </Fragment>
+  )
+}
---
+++ pages/_document.js
@@ -0,0 +1,72 @@
+import { Fragment } from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID, GTM_TRACKING_ID } from '../components/lib/gtag'
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    // const sheet = new ServerStyleSheet()
+    // const originalRenderPage = ctx.renderPage
+
+    const initialProps = await Document.getInitialProps(ctx)
+
+    // Check if in production
+    const isProduction = process.env.NODE_ENV === 'production'
+
+    return {
+      ...initialProps,
+      isProduction,
+    }
+  }
+
+  render() {
+    const { isProduction } = this.props
+
+    return (
+      <html lang="en">
+        <Head>
+
+          {/* We only want to add the scripts if in production */}
+          {isProduction && (
+            <Fragment>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+              <script dangerouslySetInnerHTML={{ __html: `<!-- Google Tag Manager -->
+                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_TRACKING_ID}');</script>
+                <!-- End Google Tag Manager -->`}} 
+              />
+            </Fragment>
+          )}
+        </Head>
+        <body>
+          <script dangerouslySetInnerHTML={{ __html: `<!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_TRACKING_ID}"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->`}}
+          />
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
